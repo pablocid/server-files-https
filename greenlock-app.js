1fe3e37621e55d9a778ab86ca7634c3d813a3c64
Pablo <pabloagronomo@gmail.com>
2019-07-25 14:34:16 -0400

    greenlock envs

--- greenlock-app.js
+++ greenlock-app.js
@@ -2,14 +2,13 @@ var homedir = require('path').join(require('os').homedir());
 require('greenlock-express').create({
 
     version: 'draft-11'
-    , server: 'https://acme-staging-v02.api.letsencrypt.org/directory'  // staging
+    , server: process.env.STAGE  // staging
     , email: 'john.doe@localhost'                                     // CHANGE THIS
     , agreeTos: true
-    , approveDomains: ['localhost']              // CHANGE THIS
+    , approveDomains: [process.env.DOMAIN]              // CHANGE THIS
     , configDir: homedir
     , app: require('./index.js')
     , store: require('greenlock-store-fs')
-
     , communityMember: true
     //, debug: true
 }).listen(4000);

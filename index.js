f4f2e9dab4ba47ea7a8418fed54ca6baa8303275
pablo <pabloagronomo@gmail.com>
2019-07-26 17:08:59 -0400

    ahora si

--- index.js
+++ index.js
@@ -6,28 +6,9 @@ const cors = require('cors');
 const privateKEY = readFileSync('./private.key', 'utf8');
 const publicKEY = readFileSync('./public.key', 'utf8');
 const app = express();
+
 // enable CORS
 app.use(cors());
-
-// SIGNING OPTIONS
-const signOptions = {
-    issuer: "SPEC",
-    subject: "pcid@spec.cl",
-    audience: "http://spec.cl/",
-    expiresIn: "30m",
-    algorithm: "RS256"
-};
-
-/*
- ====================   JWT Verify =====================
-*/
-const verifyOptions = {
-    issuer: "SPEC",
-    subject: "pcid@spec.cl",
-    audience: "http://spec.cl/",
-    expiresIn: "30m",
-    algorithm: ["RS256"]
-};
 
 // setting the enviromental variables
 const PORT = process.env.PORT;
@@ -39,8 +20,22 @@ const apiKeys = new Map();
 apiKeys.set(APIKEY, { id: 1, name: 'Api key USER' });
 
 app.use('/.well-known', express.static('./webrootPath/well-known'));
-// middleware for checking the apikey
 
+// middleware for checking the apikey
+const signOptions = {
+    issuer: "SPEC",
+    subject: "pcid@spec.cl",
+    audience: "http://spec.cl/",
+    expiresIn: "30m",
+    algorithm: "RS256"
+};
+const verifyOptions = {
+    issuer: "SPEC",
+    subject: "pcid@spec.cl",
+    audience: "http://spec.cl/",
+    expiresIn: "30m",
+    algorithm: ["RS256"]
+};
 const apiKeyHandler = (req, res, next) => {
     if (!req.query.apikey && !req.query.token) { res.status(401).send('Forbidden access'); return; }
 
@@ -59,7 +54,7 @@ const apiKeyHandler = (req, res, next) => {
     }
 
 }
-// if (process.env.PROD === "YES") { app.use(apiKeyHandler); }
+app.use(apiKeyHandler);
 
 app.use('/ameba00/files', proxy('192.168.1.100:8080'));
 app.use('/ameba00/files-node', proxy('192.168.1.100:8081'));
